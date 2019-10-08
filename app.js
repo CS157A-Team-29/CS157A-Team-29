ded066ad187a5f288d7d64b7bcbadba2cac61886
jan <rodriguez.janrichard@gmail.com>
2019-10-08 14:14:59 -0700

    Added proper entity attributes and their respective data

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 var connection = mysql.createConnection({
   host:'localhost',
   user:'root',
-  password:'', //Enter your password
+  password:'P1p3d-sql', //Enter your password
   database:'cs157a' //Enter the name of your database
 });
 
@@ -21,7 +21,7 @@ connection.connect(function(error){
 });
 
 app.get('/',function(req,res){
-  connection.query("select * from emp", function(error,result){
+  connection.query("select * from Accounts", function(error,result){
     if(!!error){
       console.log('Error in query');
     }
@@ -31,5 +31,55 @@ app.get('/',function(req,res){
     }
   });
 });
+
+app.get('/accounts',function(req,res){
+  connection.query("select * from Accounts", function(error,result){
+    if(!!error){
+      console.log('Error in query');
+    }
+    else{
+      res.render("accounts",{rows:result});
+      console.log(result);
+    }
+  });
+});
+
+app.get('/studyset',function(req,res){
+  connection.query("select * from StudySet", function(error,result){
+    if(!!error){
+      console.log('Error in query');
+    }
+    else{
+      res.render("study-set",{rows:result});
+      console.log(result);
+    }
+  });
+});
+
+app.get('/folders',function(req,res){
+  connection.query("select * from Folders", function(error,result){
+    if(!!error){
+      console.log('Error in query');
+    }
+    else{
+      res.render("folders",{rows:result});
+      console.log(result);
+    }
+  });
+});
+
+app.get('/flashcards',function(req,res){
+  connection.query("select * from Flashcard", function(error,result){
+    if(!!error){
+      console.log('Error in query');
+    }
+    else{
+      res.render("flashcards",{rows:result});
+      console.log(result);
+    }
+  });
+});
+
+
 
 app.listen(1337);
