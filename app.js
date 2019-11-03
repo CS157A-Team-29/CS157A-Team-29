493433bcc765d765aa405dac194ccb7b1d540ef1
Aaron Smith <33443869+aaronsjsu@users.noreply.github.com>
2019-11-02 22:56:51 -0700

    finished login page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express");
 let mysql = require("mysql");
 let path = require("path");
 let bodyParser = require("body-parser"); // body-parser for handling post requests
+let passwordHash = require("password-hash"); // Don't store plaintext passwords in database. Though we are sending plaintext passwords over http...
 let app = express();
 
 app.set("view engine", "ejs");
@@ -19,23 +20,68 @@ let database = mysql.createConnection({
 
 database.connect(function(error) {
   if (error) {
+	console.log("Error connecting to database");
  	console.log(error);
   } else {
 	console.log("Connected to database");
   }
 });
 
-// Load login/signup page
+// Load login/signup page by default
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/html/login.html"));
 });
 
+// Login to the platform. Check database for valid username and password.
 app.post("/login", function(req, res) {
-  console.log(req.body.username + " logging in");
+  let query = "SELECT * FROM accounts WHERE username = \"" + req.body.username + "\";";
+  			//+ "\" AND password = \"" + passwordHash.generate(req.body.password) + "\"";
+  database.query(query, function(error, result) {
+	if (error) {
+      console.log("Error in login query");
+    } else {
+	   if (result.length === 1 && passwordHash.verify(req.body.password, result[0].password)) {
+		 // If this is true, then our query returned a row in the database, i.e. the username/password combination is correct.
+		 console.log("Logging in as " + req.body.username);
+		 res.send("valid");
+	   } else {
+		 // Else username/password combination not in the database.
+		 res.send("invalid");
+	   }
+    }
+  });
 });
 
+// Sign up for the platform. Check data and database so that a valid account can be created.
 app.post("/signup", function(req, res) {
-  console.log(req.body.username + " signing up");
+  let query = "INSERT INTO accounts (username, email, password) VALUES(\"" + req.body.username + "\",\""
+  			+ req.body.email + "\",\"" + passwordHash.generate(req.body.password) + "\");";
+  database.query(query, function(error, result) {
+  	if (error) {
+      console.log("Error in signup query");
+	  console.log(error);
+    } else {
+	  res.send("valid");
+    }
+  });
+});
+
+// Checks if the username is in the database. If it isn't, returns "valid" to the client.
+app.post("/checkIfUsernameValid", function(req, res) {
+  let query = "SELECT * FROM accounts WHERE username = \"" + req.body.username + "\";";
+  database.query(query, function(error, result) {
+    if (error) {
+      console.log("Error in checkIfUsernameValid query");
+	} else {
+	  if (result.length === 1) {
+	    // If this is true, then our query returned a row in the database
+	    res.send("invalid");
+	  } else {
+		// Else username not in the database
+		res.send("valid");
+	  }
+	}
+  });
 });
 
 // Load landing page
--- public/js/login.js
+++ public/js/login.js
@@ -1,64 +1,192 @@
+// Keep track of commonly used document elements.
+let docElements = {
+  form: document.getElementById("logInForm"),
+  username: document.getElementById("username"),
+  password: document.getElementById("password")
+}
 
-// Function sends post request to server with the username and password
+
+// Function to attempt to login. Sends post request to server with the username and password
 function logIn() {
   console.log("Logging in");
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:1337/login");
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   let credentials = {
-    username: document.getElementById("username").value,
-	password: document.getElementById("password").value
+    username: docElements.username.value,
+	password: docElements.password.value
   }
   xhr.send(JSON.stringify(credentials));
+
+  xhr.responseType = "text";
+  xhr.onload = function() {
+	if (xhr.response === "valid") {
+	  // true if useername/password combination succeeded
+	  // Redirect to landing page once signed in
+	  window.location.href = "http://localhost:1337/landing";
+    } else {
+	  if (document.getElementById("errorMessage") === null) { // Make sure we don't make duplicates
+		let message = document.createElement("p");
+    	message.innerHTML = "Username/Password combination failed";
+    	message.id = "errorMessage";
+    	document.getElementById("logInForm").appendChild(message);
+	  }
+    }
+  };
 }
 
+
+// Function to attempt to sign up. Sends post request to server with the email, username, and password.
 function signUp() {
   console.log("Signing up");
-  if (document.getElementById("password").value !== document.getElementById("repeatPassword").value) {
-	console.log("passwords do not match");
+  // Make sure input is valid
+  if (docElements.passwordValid === false) {
+	console.log("Password invalid");
 	return;
+  } else if (docElements.emailValid === false) {
+ 	console.log("Email invalid");
+	return;
+  } else if (docElements.usernameValid === false) {
+	console.log("Username invalid");
+  	return;
   }
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:1337/signup");
   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   let credentials = {
-	email: document.getElementById("email").value,
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
+	email: docElements.email.value,
+    username: docElements.username.value,
+    password: docElements.password.value,
   }
+  console.log(credentials);
   xhr.send(JSON.stringify(credentials));
+
+  xhr.responseType = "text";
+  xhr.onload = function() {
+	if (xhr.response === "valid") {
+	  // true if creating new account was successful
+	  // Redirect to landing page once signed in
+	  window.location.href = "http://localhost:1337/landing";
+    }
+  };
 }
 
+
+// Switches the form to sign up page instead of login.
+// Also updates form based on user input, to display if fields are valid.
 function switchToSignUp() {
-	let emailTextField = document.createElement("input");
-	emailTextField.className = "fadeIn first";
-	emailTextField.placeholder = "email";
-	emailTextField.name = "login";
-	emailTextField.type = "text";
-	emailTextField.id = "email"
-	document.getElementById("username").className = "fadeIn second";
-	document.getElementById("password").className = "fadeIn third";
-	let repeatPasswordTextField = document.createElement("input");
-	repeatPasswordTextField.className = "fadeIn fourth";
-	repeatPasswordTextField.placeholder = "repeat password";
-	repeatPasswordTextField.name = "login";
-	repeatPasswordTextField.type = "password";
-	repeatPasswordTextField.id = "repeatPassword"
-	let signUpButton = document.createElement("input");
-	signUpButton.className = "fadeIn fifth";
-	signUpButton.value = "sign up";
-	signUpButton.id = "signUpButton";
-	signUpButton.type = "button";
-	signUpButton.addEventListener('click', signUp);
+  if (document.getElementById("errorMessage") !== null) {
+	document.getElementById("errorMessage").outerHTML = "";
+  }
 
-	let form = document.getElementById("logInForm");
-	form.removeChild(document.getElementById("switchToSignUpButton"));
-	form.removeChild(document.getElementById("logInButton"));
-	form.insertBefore(emailTextField, document.getElementById("username"));
-	form.appendChild(repeatPasswordTextField);
-	form.appendChild(signUpButton);
+  let emailTextField = document.createElement("input");
+  emailTextField.className = "fadeIn first";
+  emailTextField.placeholder = "email";
+  emailTextField.name = "login";
+  emailTextField.type = "text";
+  emailTextField.id = "email";
+  docElements.email = emailTextField;
+  let repeatPasswordTextField = document.createElement("input");
+  repeatPasswordTextField.className = "fadeIn second";
+  repeatPasswordTextField.placeholder = "repeat password";
+  repeatPasswordTextField.name = "login";
+  repeatPasswordTextField.type = "password";
+  repeatPasswordTextField.id = "repeatPassword"
+  docElements.repeatPassword = repeatPasswordTextField;
+  let signUpButton = document.createElement("input");
+  signUpButton.className = "fadeIn third";
+  signUpButton.value = "sign up";
+  signUpButton.id = "signUpButton";
+  signUpButton.type = "button";
+  signUpButton.addEventListener('click', signUp);
+  let emailStatusSymbol = document.createElement("span");
+  emailStatusSymbol.id = "emailStatusSymbol";
+  emailStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+  let usernameStatusSymbol = document.createElement("span");
+  usernameStatusSymbol.id = "usernameStatusSymbol";
+  usernameStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+  let passwordStatusSymbol = document.createElement("span");
+  passwordStatusSymbol.id = "passwordStatusSymbol";
+  passwordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+  let repeatPasswordStatusSymbol = document.createElement("span");
+  repeatPasswordStatusSymbol.id = "repeatPasswordStatusSymbol";
+  repeatPasswordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+
+  docElements.form.removeChild(document.getElementById("switchToSignUpButton"));
+  docElements.form.removeChild(document.getElementById("logInButton"));
+  docElements.form.insertBefore(emailTextField, docElements.username);
+  docElements.form.appendChild(repeatPasswordTextField);
+  docElements.form.appendChild(signUpButton);
+  docElements.form.insertBefore(emailStatusSymbol, docElements.username);
+  docElements.form.insertBefore(usernameStatusSymbol, docElements.password);
+  docElements.form.insertBefore(passwordStatusSymbol, docElements.repeatPassword);
+  docElements.form.insertBefore(repeatPasswordStatusSymbol, signUpButton);
+
+  docElements.emailValid = false;
+  docElements.usernameValid = false;
+  docElements.passwordValid = false;
+
+  // Constantly checks if email is valid
+  docElements.email.oninput = function() {
+    // Use this regex to test if email is valid. Found at: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+	let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(String(docElements.email.value).toLowerCase())) {
+	  emailStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-ok\"></span>";
+	  docElements.emailValid = true;
+    } else {
+	  emailStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+	  docElements.emailValid = false;
+    }
+  }
+
+  // Constantly checks username input to see if it's valid
+  docElements.username.oninput = function() {
+	// Check if username is valid by querying database
+	let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:1337/checkIfUsernameValid");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	let data = {
+	  username: docElements.username.value
+	}
+	xhr.send(JSON.stringify(data));
+    xhr.responseType = "text";
+    xhr.onload = function() {
+	  // Put an icon after username indicating if it's valid or not.
+  	  if (xhr.response === "valid") {
+  	    // true if username doesn't already exist in the database
+		usernameStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-ok\"></span>";
+		docElements.usernameValid = true;
+	  } else if (xhr.response === "invalid") {
+		// true if username already exists in the database
+		usernameStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+		docElements.usernameValid = false;
+	  }
+    };
+  }
+
+  // Constantly checks if password input is valid. Should be 5 characters long or more.
+  docElements.password.oninput = function() {
+    if (docElements.password.value.length >= 5) {
+	  passwordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-ok\"></span>";
+    } else {
+	  passwordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+    }
+  }
+
+  // Constantly checks if repeat password input is valid. Should match the password from the first password field
+  docElements.repeatPassword.oninput = function() {
+    if (docElements.repeatPassword.value.length >= 5 && docElements.repeatPassword.value === docElements.password.value) {
+	  repeatPasswordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-ok\"></span>";
+	  docElements.passwordValid = true;
+    } else {
+	  repeatPasswordStatusSymbol.innerHTML = "<span class=\"glyphicon glyphicon-remove\"></span>";
+	  docElements.passwordValid = false;
+    }
+  }
 }
 
+
+// Assign our functions to run on button press.
 document.querySelector("#logInButton").addEventListener('click', logIn);
 document.querySelector("#switchToSignUpButton").addEventListener('click', switchToSignUp);
